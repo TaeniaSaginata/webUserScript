20b68d93100af3e09f26954e11f785ff5a7f9882
Arlinda Struzl <arlinda@Tapetop.taenia>
2024-10-27 12:59:45 +0100

    ~use svg as mode indicator

--- scripts/stringFormatHelpers.js
+++ scripts/stringFormatHelpers.js
@@ -1,9 +1,11 @@
 function capitalizeFirstLetter(word) {
     return word.slice(0, 1).toUpperCase() + word.slice(1);
 }
+
 function firstLetterToLowerCase(word) {
     return word.slice(0, 1).toLowerCase() + word.slice(1);
 }
+
 function toKebabCase(word) {
     return [word[0].toLowerCase(), ...word.split('').slice(1).map(letter => /[A-Z]/.test(letter) ? `-${letter.toLowerCase()}` : letter.toLowerCase())].join('');
 }
